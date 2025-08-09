cd14094e58222adf88c5b73b6935888818931175
Rob Pilling <robpilling@gmail.com>
2025-08-09 16:46:59 +0100

    Remove command debugging

--- lua/cartographer.lua
+++ lua/cartographer.lua
@@ -51,74 +51,59 @@ end
 
 local function hook_cmds()
 	local cmds = vim.api.nvim_get_commands({})
-	local DEBUG = false
 
 	table.sort(cmds)
 	for i, cmd in pairs(cmds) do
 		-- TODO: handle cmd.buffer
 
 		cmd.nargs = tonumber(cmd.nargs)
-		if false and (cmd.nargs == 0 or cmd.nargs == '0') then
-			print("TODO: nargs=0 for " .. cmd.name)
-		else
-			if DEBUG then print("cartographer: wrapping '" .. cmd.name .. "'") end
 
-			if cmd.range == "." then
-				cmd.range = nil -- -range
-			elseif cmd.range == "%" then
-				cmd.range = "%" -- -range=%
-			elseif cmd.range ~= nil then
-				local n = cmd.range:match("(%d+)$") -- -count=N
-				if n ~= nil then
-					cmd.range = nil
-					cmd.count = tonumber(n)
-				else
-					cmd.range = "-range=" .. cmd.range
-				end
+		if cmd.range == "." then
+			cmd.range = nil -- -range
+		elseif cmd.range == "%" then
+			cmd.range = "%" -- -range=%
+		elseif cmd.range ~= nil then
+			local n = cmd.range:match("(%d+)$") -- -count=N
+			if n ~= nil then
+				cmd.range = nil
+				cmd.count = tonumber(n)
+			else
+				cmd.range = "-range=" .. cmd.range
 			end
-			if cmd.range ~= nil then
-				if DEBUG then vim.cmd('echom "  ' .. cmd.name .. "'s range is " .. cmd.range .. ', count is ' .. (cmd.count or "n/a") .. '"') end
-			end
-
-			if cmd.complete ~= nil and cmd.nargs == nil then
-				cmd.nargs = '*'
-				if DEBUG then vim.cmd('echom "  ' .. cmd.name .. ': setting nargs to ' .. cmd.nargs .. '"') end
-			end
-			if cmd.complete ~= nil and cmd.complete:match("^custom") then
-				cmd.complete = cmd.complete .. "," .. cmd.complete_arg
-				if DEBUG then
-					if DEBUG then vim.cmd('echom "  ' .. cmd.name .. ": completion is " .. cmd.complete .. '"') end
-				end
-			end
-
-			if DEBUG then vim.cmd("echom '  nvim_create_user_command(" .. vim.inspect(cmd):gsub("\n", "\\n"):gsub("'", "\"") .. ")'") end
-
-			vim.api.nvim_create_user_command(
-				cmd.name,
-				function(details)
-					-- details.{name,args,fargs,nargs,bang,line1,line2,range,count,reg,mods,smods}
-
-					-- TODO: test
-					vim.cmd.echom('"' .. cmd.definition:gsub('"', '\\"') .. '"')
-					vim.cmd(cmd.definition)
-				end,
-				{
-					force = true,
-
-					addr = cmd.addr,
-					bang = cmd.bang,
-					bar = cmd.bar,
-					complete = cmd.complete,
-					--complete_arg = cmd.complete_arg, -- bundled as part of cmd
-					count = cmd.count,
-					keepscript = cmd.keepscript,
-					nargs = cmd.nargs,
-					--preview = cmd.preview, -- FIXME: boolean comes through???
-					range = cmd.range,
-					register = cmd.register,
-				}
-			)
 		end
+
+		if cmd.complete ~= nil and cmd.nargs == nil then
+			cmd.nargs = '*'
+		end
+		if cmd.complete ~= nil and cmd.complete:match("^custom") then
+			cmd.complete = cmd.complete .. "," .. cmd.complete_arg
+		end
+
+		vim.api.nvim_create_user_command(
+			cmd.name,
+			function(details)
+				-- details.{name,args,fargs,nargs,bang,line1,line2,range,count,reg,mods,smods}
+
+				-- TODO: test
+				vim.cmd.echom('"' .. cmd.definition:gsub('"', '\\"') .. '"')
+				vim.cmd(cmd.definition)
+			end,
+			{
+				force = true,
+
+				addr = cmd.addr,
+				bang = cmd.bang,
+				bar = cmd.bar,
+				complete = cmd.complete,
+				--complete_arg = cmd.complete_arg, -- bundled as part of cmd
+				count = cmd.count,
+				keepscript = cmd.keepscript,
+				nargs = cmd.nargs,
+				--preview = cmd.preview, -- FIXME: boolean comes through???
+				range = cmd.range,
+				register = cmd.register,
+			}
+		)
 	end
 end
 
