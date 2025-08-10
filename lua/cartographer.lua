bee786e144459567fe2458b9666be12818ffcc01
Rob Pilling <robpilling@gmail.com>
2025-08-10 21:02:06 +0100

    remap -> boolean

--- lua/cartographer.lua
+++ lua/cartographer.lua
@@ -49,7 +49,7 @@ local function hook_keymaps()
 	local keymap = vim.api.nvim_get_keymap('')
 
 	for i, mapping in pairs(keymap) do
-		local remap = mapping.noremap and "n" or ""
+		local remap = nil_or_zero(mapping.noremap)
 
 		if mapping.rhs ~= nil and mapping.mode:gsub("%s+", ""):len() > 0 then
 			local scriptpath = scriptname(mapping.sid, true)
@@ -105,7 +105,11 @@ local function hook_keymaps()
 							out = vim.api.nvim_replace_termcodes(mapping.rhs, true, true, true)
 						end
 
-						vim.api.nvim_feedkeys(out, remap, false)
+						vim.api.nvim_feedkeys(
+							out,
+							remap and "" or "n",
+							false -- escape K_SPECIAL
+						)
 					end,
 				}
 			)
